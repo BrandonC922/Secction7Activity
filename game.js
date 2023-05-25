c355cd78160ca89c940ce81ecec2b119f00bb402
venguardk <venguardk@users.noreply.github.com>
2023-05-25 13:02:40 -0700

    added final scene with anim

--- game.js
+++ game.js
@@ -1,11 +1,4 @@
-class Final extends Phaser.Scene {
-     constructor(){
-        super('Final');
-     }
-     create(){
-          this.add.image(1920*.5,1080*.5,"end");
-     }
-}
+
 class Title extends Phaser.Scene {
     constructor(){
         super('Title');
@@ -14,6 +7,8 @@ class Title extends Phaser.Scene {
         this.load.path = './assets/';
         this.load.image('titleScene', 'Title.png');
         this.load.image('end', "Victory.jpg");
+        this.load.spritesheet('roly', 'RolySpriteSheet.png', {frameWidth: 100, frameHeight: 100, endFrame:2});
+
     }
     create() {
         this.add.image(1920*.5,1080*.5,"titleScene").setScale(1.9);
@@ -21,6 +16,24 @@ class Title extends Phaser.Scene {
         this.input.on('pointerdown', () => this.scene.start('Final'));
     }
 }
+
+class Final extends Phaser.Scene {
+    constructor(){
+       super('Final');
+    }
+    create(){
+         this.add.image(1920*.5,1080*.5,"end");
+         this.anims.create({
+            key:'roll',
+            frames:'roly',
+            frameRate: 6,
+            repeat: -1
+         });
+
+         const sprite1 = this.add.sprite(1920*.5, 1080*.5, 'roly').play('roll');
+    }
+}
+
 const game = new Phaser.Game({
     scale: {
         mode: Phaser.Scale.FIT,
